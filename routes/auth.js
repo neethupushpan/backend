b477694b77b0981c5d75e4e8dea2ba7e5cdf5685
neethupushpan <36188563+neethupushpan@users.noreply.github.com>
2025-04-25 23:34:42 +0530

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 import express from 'express';
 const bcrypt = require('bcryptjs');
-import express from "express";
+
 import bcrypt from "bcryptjs";
 import jwt from 'jsonwebtoken';
 import User from '../models/User.js';
